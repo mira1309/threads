8ecaeb2a0137b8790e16c598777e70e505f8ff44
mira1309 <134642665+mira1309@users.noreply.github.com>
2024-04-16 17:39:27 +0300

    test with SpringBootTest

--- src/test/java/ru/hogwarts/school/controller/FacultyControllerTest.java
+++ src/test/java/ru/hogwarts/school/controller/FacultyControllerTest.java
@@ -1,17 +1,12 @@
 package ru.hogwarts.school.controller;
 
 import org.assertj.core.api.Assertions;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import ru.hogwarts.school.model.Faculty;
-
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
--- src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
+++ src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
@@ -1,17 +1,13 @@
 package ru.hogwarts.school.controller;
 
 import org.assertj.core.api.Assertions;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import ru.hogwarts.school.controller.StudentController;
 import ru.hogwarts.school.model.Student;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
