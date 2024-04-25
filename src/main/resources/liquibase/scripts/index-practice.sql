b75f8af2c73eb64e87ce3e08b7e91d2b1914ce3b
mira1309 <134642665+mira1309@users.noreply.github.com>
2024-04-25 17:35:40 +0300

    Подключена зависимость liquibase-core. Создан пустой файл миграций. Настроен файл changelog-master.yml. В файле миграций созданы два changeset с SQL-командами для создания индексов.

--- src/main/resources/liquibase/scripts/index-practice.sql
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -2,4 +2,4 @@
 
 -- changeset mira:1
 CREATE INDEX student_name_index ON student(name);
-CREATE INDEX faculty_name_color_index ON faculty(name, color);
+CREATE INDEX faculty_name_and_color_index ON faculty(name, color);
